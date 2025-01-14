284a06df2f9911d87239ebea55473548d1fa6339
JAYLORD ANDRADA LAGUD <124552983+jaylordlagud2022@users.noreply.github.com>
2025-01-14 20:30:02 +0800

    Setup css and script file and directory

--- wordpress/wp-content/plugins/custom-woocommerce-plugin/wc-banner-manager.php
+++ wordpress/wp-content/plugins/custom-woocommerce-plugin/wc-banner-manager.php
@@ -5,3 +5,19 @@
  * Version: 1.0
  * Author: Jaylord Lagud
  */
+
+ if (!defined('ABSPATH')) {
+     exit; // Exit if accessed directly
+ }
+
+ // Define constants
+ define('WC_BANNER_MANAGER_PATH', plugin_dir_path(__FILE__));
+ define('WC_BANNER_MANAGER_URL', plugin_dir_url(__FILE__));
+
+ // Enqueue scripts and styles
+ function wc_banner_manager_enqueue_assets() {
+     wp_enqueue_style('wc-banner-manager-style', WC_BANNER_MANAGER_URL . 'assets/css/style.css');
+     wp_enqueue_script('wc-banner-manager-script', WC_BANNER_MANAGER_URL . 'assets/js/script.js', ['jquery'], null, true);
+ }
+ add_action('admin_enqueue_scripts', 'wc_banner_manager_enqueue_assets');
+ add_action('wp_enqueue_scripts', 'wc_banner_manager_enqueue_assets');
